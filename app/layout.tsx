f4f7965add9434ad3e401d31f6b8b68a52d371e0
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-07-21 22:49:23 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Waevo Hosting",
+  description: "Hosting de bots para Discord",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+          <Toaster position="bottom-right" />
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
